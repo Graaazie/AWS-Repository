dabbfaa6904bf307b074770c1f4aa51fe54fb39d
Graaazie <dragon063185@gmail.com>
2025-11-10 12:13:27 +0900

    Update TEST_WAF_DVWA.sh

--- TEST_WAF_DVWA.sh
+++ TEST_WAF_DVWA.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# MySQL 변수 정의
-DB_NAME="dvwa"
+DB_NAME="mytestdb"
 DB_USER="dvwa"
 USER_PWD="p@ssw0rd"
+DB_Admin="admin"
+Admin_PWD="password"
+DB_HOST="waf-db.cp8was4ksowb.ap-northeast-2.rds.amazonaws.com"
 
-# SQL 명령어 정의
 SQL_COMMANDS=$(cat <<EOF
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${USER_PWD}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${USER_PWD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 )
 
-# MySQL 접속 및 실행
-mysql -uroot -e "${SQL_COMMANDS}"
+sed -i "s/=getenv('DB_SERVER') ?: '127.0.0.1';/=getenv('DB_SERVER') ?: '${DB_HOST}';/g" /var/www/html/config/config.inc.php
+sed -i "s/=getenv('DB_DATABASE') ?: 'dvwa';/=getenv('DB_DATABASE') ?: '${DB_NAME}';/g" /var/www/html/config/config.inc.php
+sed -i "s/=getenv('DB_USER') ?: 'dvwa';/=getenv('DB_USER') ?: '${DB_USER}';/g" /var/www/html/config/config.inc.php
+sed -i "s/=getenv('DB_PASSWORD') ?: 'p@ssw0rd';/=getenv('DB_PASSWORD') ?: '${USER_PWD}';/g" /var/www/html/config/config.inc.php
+
+mysql -h ${DB_HOST} -u ${DB_Admin} -p${Admin_PWD} ${DB_Name} -e "${SQL_COMMANDS}"
