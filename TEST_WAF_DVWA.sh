d469242cbef93dd25435355b247cdc34902beff6
Graaazie <dragon063185@gmail.com>
2025-11-10 12:45:16 +0900

    Update TEST_WAF_DVWA.sh

--- TEST_WAF_DVWA.sh
+++ TEST_WAF_DVWA.sh
@@ -14,9 +14,9 @@ FLUSH PRIVILEGES;
 EOF
 )
 
-sed -i ": '127.0.0.1'/: '${DB_HOST}'/g" /var/www/html/config/config.inc.php
-sed -i ": 'dvwa'/: '${DB_NAME}'/g" /var/www/html/config/config.inc.php
-sed -i ": 'dvwa'/: '${DB_USER}'/g" /var/www/html/config/config.inc.php
-sed -i ": 'p@ssw0rd'/: '${USER_PWD}'/g" /var/www/html/config/config.inc.php
+sed -i "s|getenv('DB_SERVER') ?: '127.0.0.1';|getenv('DB_SERVER') ?: '${DB_HOST}';|g" /var/www/html/config/config.inc.php
+sed -i "s|getenv('DB_DATABASE') ?: 'dvwa';|getenv('DB_DATABASE') ?: '${DB_NAME}';|g" /var/www/html/config/config.inc.php
+sed -i "s|getenv('DB_USER') ?: 'dvwa';|getenv('DB_USER') ?: '${DB_USER}';|g" /var/www/html/config/config.inc.php
+sed -i "s|getenv('DB_PASSWORD') ?: 'p@ssw0rd';|getenv('DB_PASSWORD') ?: '${USER_PWD}';|g" /var/www/html/config/config.inc.php
 
 mysql -h ${DB_HOST} -u ${DB_Admin} -p${Admin_PWD} ${DB_NAME} -e "${SQL_COMMANDS}"
