d5d56e12e5ebbaf1546039cbb159f7112d92d7cf
Graaazie <dragon063185@gmail.com>
2025-11-10 15:20:21 +0900

    Update nfs-server.sh

--- nfs-server.sh
+++ nfs-server.sh
@@ -4,7 +4,7 @@ mkdir /shared
 chmod 777 /shared
 echo "/shared 172.16.*(rw,sync)" > /etc/exports
 systemctl stop firewalld
-ec2-metadata -o | cut -d ' ' -f 1 >> /shared/list.txt
+ec2-metadata -o | cut -d ' ' -f 2 >> /shared/list.txt
 chmod 777 /shared/list.txt
 
 systemctl start nfs-server
